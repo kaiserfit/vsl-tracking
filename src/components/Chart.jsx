b9fe7fd3ff654de6f3ec8ced0d29ea7fe27d5b9e
KaiserFit <108109987+kaiserfit@users.noreply.github.com>
2022-09-19 15:12:55 +0800

    12

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -35,16 +35,60 @@ import {
     const [minsPlayed, setMinsPlayed] = useState(0);
     const [chartData, setChartData] = useState(null);
     const [chartLabel, setChartLabel] = useState(null);
+    const [chartOptions, setChartOptions] = useState(null);
+    const [chartConfigData, setChartConfigData] = useState(null);
     const vslLength = 2957;
- 
+    
+
+    const options = {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: 'Vsl Watch Statistics Over Time Progression',
+          },
+        },
+        scales:{
+            x: {
+                display: false
+            },
+            y: {
+                ticks: { color: 'black', beginAtZero: true }
+              },
+
+        }
+      };
+
+      const data = {
+        labels: chartLabel,
+        datasets: [
+          {
+            label: 'Watchers',
+            data: chartData,
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            
+          },
+        //   {
+        //     label: 'Dataset 2',
+        //     data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+        //     borderColor: 'rgb(53, 162, 235)',
+        //     backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        //   },
+        ],
+      };
     useEffect(()=>{
         GetData('', '');
     }, []);
-
+    
     useEffect(()=>{
         if (dataLoaded){
-            console.log(chartData)
-            console.log(chartLabel)
+            const t1 = vslPlays * vslLength;
+            const eg = minsPlayed / t1;
+            setEngagementRate(eg.toFixed(2))
         }
     }, [dataLoaded]);
     
@@ -83,6 +127,46 @@ import {
         });
     }
 
+    function DataSection() {
+        return (
+          <div className="grid grid-rows-2 grid-flow-col gap-4 mt-10 justify-items-center">
+          <div className="text-2xl">Landings:  {vslLands}</div>
+          <div className="text-2xl">Plays: {vslPlays}</div>
+          <div className="text-2xl">Play Rate: <span className="text-green-600">{((vslPlays/vslLands)*100).toFixed(2) + '%'}</span></div>
+          <div className="text-2xl">Total Minutes Played: {minsPlayed / 60} mins</div>
+          <div className="text-2xl">Engagement Rate: <span className="text-green-600">{engagementRate}%</span></div>
+          <div className="text-2xl">VSL Duration: {(vslLength / 60).toFixed(2)} mins</div>
+  
+            </div>
+        )
+      }
+    const LoadingMessage = () => {
+        return(
+          <h1 className="text-center text-3xl inline-flex mt-4">
+            Fetching Data..<AiOutlineLoading3Quarters className="spinner" />
+          </h1>
+        )
+      }
+
+
+    function VslChart(){ 
+
+        return (  
+          <>
+        <div className="container h-auto p-4 w-3/4 mx-auto mt-5 align-middle rounded-2xl bg-gradient-to-r from-indigo-500">
+      
+            <Line options={options} data={data} className="h-auto w-auto object-contain" />
+        </div>
+    
+          {(dataLoaded===true)  ? <DataSection /> : <LoadingMessage />}
+    
+          </>
+        );
+
+
+  
+}
+
 
     return (
         <>
@@ -102,6 +186,8 @@ import {
                 </div>
             </div>
         </div>
+
+        <VslChart />
         </>
     )
   }
